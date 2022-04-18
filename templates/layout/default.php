3ccc4c1db636148cc90f0a72c8b4a27e62942ecb
Graham Watson <graham@phantomwatson.com>
2022-04-18 01:38:28 -0400

    Update styling

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -13,41 +13,39 @@
         <?= $title ?>
     </title>
     <?= $this->Html->meta('icon') ?>
-    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/css/styles.css" rel="stylesheet" />
-    <link href="/fontawesome/css/all.css" rel="stylesheet">
-    <?php /* echo $this->Html->css(['normalize.min', 'milligram.min', 'cake']);*/ ?>
-    <script src="/js/vgr-fetcher.js"></script>
+    <?= $this->Html->css('/bootstrap/css/bootstrap.min.css') ?>
+    <?= $this->Html->css('/css/styles.css') ?>
+    <?= $this->Html->css('/fontawesome/css/all.css') ?>
+    <?= $this->Html->script('/js/vgr-fetcher.js') ?>
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <h1>
-        VGR Album Art Fetcher
-    </h1>
+
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <h1 class="navbar-brand mb-0 h1">
+                VGR Album Art Fetcher
+            </h1>
+        </div>
+    </nav>
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
         </div>
     </main>
-    <footer>
-        <ul>
-            <li>
-                &copy; <?= date('Y') ?> <a href="https://phantomwatson.com">Phantom Watson</a>
-            </li>
-            <li>
-                <a href="https://musicbrainz.org/">
-                    MusicBrainz.org
-                </a>
-            </li>
-            <li>
-                <a href="https://coverartarchive.org">
-                    CoverArtArchive.org
-                </a>
-            </li>
-        </ul>
+    <footer class="container">
+        &copy; <?= date('Y') ?> <a href="https://phantomwatson.com">Phantom Watson</a>
+        -
+        <a href="https://musicbrainz.org/">
+            MusicBrainz.org
+        </a>
+        -
+        <a href="https://coverartarchive.org">
+            CoverArtArchive.org
+        </a>
     </footer>
 </body>
 </html>
